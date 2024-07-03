0ec9a891592b467bd062f734ced8f1a2ecf1e0ed
DUNGCT\ndung <ndung983@gmail.com>
2024-07-03 21:48:38 +0700

    ok

--- ExpressPortfolioMy/index.js
+++ ExpressPortfolioMy/index.js
@@ -5,6 +5,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 // Thiết lập multer để lưu file vào thư mục 'images'
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
